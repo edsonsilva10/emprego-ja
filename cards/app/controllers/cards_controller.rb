144686ee3096758680d01a2333d643c23f7f8a2a
Edson Silva <edson.silva@vagas.com.br>
2016-08-25 17:19:18 -0300

    segunda semana

--- cards/app/controllers/cards_controller.rb
+++ cards/app/controllers/cards_controller.rb
@@ -1,20 +1,43 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:show,:edit,:update,:destroy]
+
   def show
-   @card = Card.find(params[:id])
   end
-  
+
   def index
     @cards = Card.all
-  end  
+  end
 
   def new
-     
+     @card = Card.new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @card.delete()
+    redirect_to cards_url
+  end
+
+  def update
+    @card.update(card_parameters)
+    redirect_to @card
   end
 
   def create
-    card =  Card.new(front: params[:frente], back: params[:verso])
+    @card = Card.new(card_parameters)
     card.save
-    redirect_to  card
+    redirect_to  @card
+  end
+
+  private
+  def card_parameters
+    params.require(:card).permit(:front,:back)
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
   end
 
 end
--- cards/config/routes.rb
+++ cards/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   # get "/",to: "home#index", as: "home"
-  resources :cards, only: [:show, :index, :new, :create] 
+  resources :cards, only: [:show, :index, :new, :create, :edit, :update, :destroy] 
 end
